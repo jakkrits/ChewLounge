e7806e6a730387625b662e34b3d491a1dc195420
jakkrits <jakk.sutt@gmail.com>
2017-07-20 12:33:55 +0700

    add auth0

---
+++ components/LoginAuth0.js
@@ -0,0 +1,8 @@
+// import React, { Component, PropTypes } from 'react';
+// import Auth0Lock from 'auth0-lock';
+//
+// class LoginAuth0 extends Component {
+//   render() {
+//     return <h1>LoginAuth0</h1>;
+//   }
+// }
